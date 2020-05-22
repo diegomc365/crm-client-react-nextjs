90e15e10a53e99a3d39e54769fbf6f84e7fd74de
Diego Matienzo <diegomc365@hotmail.com>
2020-05-22 16:52:43 -0500

    create first views

---
+++ crm-client-react-nextjs/components/Sidebar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Sidebar = () => {
+    return (
+        <aside className="bg-gray-800 sm:w-1/3 xl:w-1/5 sm:min-h-screen p-5 ">
+            <div>
+                <p className="text-white text-2xl font-black">CRM Clientes</p>
+            </div>
+        </aside>
+    )
+}
+
+export default Sidebar;
---
+++ crm-client-react-nextjs/pages/pedidos.js
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+import Layout from '../components/Layout'
+
+const Pedidos = () => (
+    <div>
+        <Layout>
+            <h1 className="text-2xl text-gray-800 font-light">Pedidos</h1>
+        </Layout>
+    </div>
+)
+
+export default Pedidos;
