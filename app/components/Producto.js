93833f61b603f9529dfad9b05067660ae3cbb673
Diego Matienzo <diegomc365@hotmail.com>
2020-06-09 23:19:19 -0500

    add delete producto

---
+++ app/components/Producto.js
@@ -0,0 +1,104 @@
+import React from 'react'
+import Swal from 'sweetalert2'
+import { gql, useMutation} from '@apollo/client'
+
+const ELIMINAR_PRODUCTO = gql`
+    mutation eliminarProducto($id: ID!){
+        eliminarProducto(id:$id)
+    }
+`;
+
+const OBTENER_PRODUCTOS = gql`
+    query obtenerProductos {
+        obtenerProductos {
+            id
+            nombre
+            precio
+            existencia
+            creado
+        }
+    }
+`;
+
+const Producto = ({producto}) => {
+    const {  nombre, precio, existencia, id } = producto;
+
+    // Mutation para eliminar productos
+    const [ eliminarProducto ] = useMutation(ELIMINAR_PRODUCTO, {
+        update(cache) {
+            const { obtenerProductos } = cache.readQuery({
+                query: OBTENER_PRODUCTOS
+            });
+
+            cache.writeQuery({
+                query: OBTENER_PRODUCTOS,
+                data: {
+                    obtenerProductos: obtenerProductos.filter( productoActual => productoActual.id !== id)
+                }
+            })
+        }
+    })
+
+    const confirmarEliminarProducto = () => {
+        Swal.fire({
+            title: 'Deseas eliminar a este producto?',
+            text: "Esta acción no se puede deshacer!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Si, Eliminar',
+            cancelButtonText: 'No, Cancelar'
+          }).then( async (result) => {
+                if (result.value) {
+                    try {
+                        // eliminar producto de la BD
+                        const {data} = await eliminarProducto({
+                            variables: {
+                                id
+                            }
+                        });
+                        // console.log(data)
+
+                        Swal.fire(
+                            'Correcto',
+                            data.eliminarProducto,
+                            'success'
+                        )
+                    } catch (error) {
+                        console.log(error)
+                    }
+                }
+          })
+    }
+
+    return (
+        <tr>
+            <td className="border px-4 py-2">{nombre}</td>
+            <td className="border px-4 py-2">{existencia} Piezas</td>
+            <td className="border px-4 py-2">S/. {precio}</td>
+            <td className="border px-4 py-2">
+                <button
+                    type="button"
+                    className="flex justify-center items-center bg-red-800 py-2 px-4 w-full text-white rounded text-xs uppercase font-bold"
+                    onClick={ () => confirmarEliminarProducto() }
+                >
+                    Eliminar
+                    <svg fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" viewBox="0 0 24 24" stroke="currentColor" className="w-4 h-4 ml-2"><path d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+                </button>
+            </td>
+            <td className="border px-4 py-2">
+                <button
+                    type="button"
+                    className="flex justify-center items-center bg-green-600 py-2 px-4 w-full text-white rounded text-xs uppercase font-bold"
+                    // onClick={ () => editarCliente() }
+                >
+                    Editar
+                    <svg fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" viewBox="0 0 24 24" stroke="currentColor" className="w-4 h-4 ml-2"><path d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"></path></svg>
+                </button>
+            </td>
+        </tr>
+    )
+}
+
+export default Producto;
--- app/pages/productos.js
+++ app/pages/productos.js
@@ -1,12 +1,57 @@
-import Head from 'next/head'
 import Layout from '../components/Layout'
+import Producto from '../components/Producto'
+import {gql, useQuery} from '@apollo/client'
 
-const Productos = () => (
-    <div>
-        <Layout>
-            <h1 className="text-2xl text-gray-800 font-light">Productos</h1>
-        </Layout>
-    </div>
-)
+const OBTENER_PRODUCTOS = gql`
+    query obtenerProductos {
+        obtenerProductos {
+            id
+            nombre
+            precio
+            existencia
+            creado
+        }
+    }
+`;
+
+const Productos = () => {
+
+    // consultar los productos
+    const { data, loading, error } = useQuery(OBTENER_PRODUCTOS)
+
+    // console.log(data)
+    // console.log(loading)
+    // console.log(error)
+
+    if(loading) return 'Cargando...'
+
+    return (
+        <div>
+            <Layout>
+                <h1 className="text-2xl text-gray-800 font-light">Productos</h1>
+                <table className="table-auto shadow-md mt-10 w-full w-lg">
+                    <thead className="bg-gray-800">
+                        <tr className="text-white" >
+                            <th className="w-1/5 py-2">Nombre</th>
+                            <th className="w-1/5 py-2">Existencia</th>
+                            <th className="w-1/5 py-2">Precio</th>
+                            <th className="w-1/5 py-2">Eliminar</th>
+                            <th className="w-1/5 py-2">Editar</th>
+                        </tr>
+                    </thead>
+
+                    <tbody className="bg-white">
+                        {data.obtenerProductos.map( producto => (
+                            <Producto 
+                            key={producto.id}
+                            producto={producto}
+                            />
+                        ))}
+                    </tbody>
+                </table>
+            </Layout>
+        </div>
+    )
+}
 
 export default Productos;
